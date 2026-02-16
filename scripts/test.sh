94ddaaa141cbea48040d19c5d3b2eb35daa9c235
Toufiq <toufiqqureshi651@gmail.com>
2026-02-16 12:12:22 +0530

    ci: fix test.sh to correctly forward arguments to pytest

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,10 +2,8 @@
 
 set -e
 
-command=( "$@" )
-if [ "${#command[@]}" -eq 0 ]; then
-  command=( "pytest --cov=chuscraper --cov-report=xml" )
-fi
+# Default command is pytest with coverage
+pytest_cmd="pytest --cov=chuscraper --cov-report=xml"
 
 chrome_executable=$(uv run python -c "from chuscraper.core.config import find_executable;print(find_executable())")
 echo "Chrome executable: $chrome_executable"
@@ -14,4 +12,4 @@ chrome_version=$(uv run python -c "import os, subprocess, sys; path = r'$chrome_
 echo "Chrome version: $chrome_version"
 
 set -x
-uv run ${command[@]}
+uv run $pytest_cmd "$@"
