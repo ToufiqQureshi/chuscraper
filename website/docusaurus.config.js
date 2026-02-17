392c5f409432c57f9ed0cc057bd379d1e846fcf8
Toufiq <toufiqqureshi651@gmail.com>
2026-02-17 17:49:16 +0530

    config: update docusaurus for Vercel deployment (baseUrl: /)

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://ToufiqQureshi.github.io',
+  url: 'https://chuscraper.vercel.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub Pages deployment, it is often '/<projectName>/'
-  baseUrl: '/chuscraper/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
